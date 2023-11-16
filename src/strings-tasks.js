31af4e66055873217e4dded1eb58e26d2b874ec0
Elvira <sabirova.elvira.gam@gmail.com>
2023-11-16 15:14:41 +0500

    feat: solve findLongestWord

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -347,8 +347,15 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  let w = '';
+  sentence
+    .replace(/[^\w\s]/g, '')
+    .split(' ')
+    .forEach((i) => {
+      w = w.length >= i.length ? w : i;
+    });
+  return w;
 }
 
 /**
